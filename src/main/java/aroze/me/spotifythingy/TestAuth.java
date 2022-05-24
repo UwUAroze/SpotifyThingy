be5eef3e85f4753f3fcd87417aca862d9fbd3bbf
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-24 22:15:16 +0100

    Formatted auth feedback

--- src/main/java/aroze/me/spotifythingy/TestAuth.java
+++ src/main/java/aroze/me/spotifythingy/TestAuth.java
@@ -1,5 +1,6 @@
 package aroze.me.spotifythingy;
 
+import aroze.me.spotifythingy.util.ChatUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,8 +13,10 @@ public class TestAuth implements CommandExecutor {
         String client_id = SpotifyThingy.getInstance().getConfig().getString("Spotify.clientID");
         String client_secret = SpotifyThingy.getInstance().getConfig().getString("Spotify.clientSecret");
 
-        sender.sendMessage("Client ID: " + client_id);
-        sender.sendMessage("Client Secret: " + client_secret);
+        sender.sendMessage(ChatUtils.color("\n&7&oAttempting to authenticate with Spotify...\n"));
+        sender.sendMessage(ChatUtils.color("&cClient ID: &7" + client_id));
+        sender.sendMessage(ChatUtils.color("&cClient Secret: &7" + client_secret));
+        sender.sendMessage("\n");
 
         return true;
     }
