84a17525ab6f3ffbd90601033b0dcdd022300112
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-24 21:40:58 +0100

    Added getInstance() method

--- src/main/java/aroze/me/spotifythingy/SpotifyThingy.java
+++ src/main/java/aroze/me/spotifythingy/SpotifyThingy.java
@@ -7,13 +7,15 @@ public final class SpotifyThingy extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
         getCommand("testauth").setExecutor(new TestAuth());
-
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static SpotifyThingy getInstance() {
+        return getPlugin(SpotifyThingy.class);
     }
 }
