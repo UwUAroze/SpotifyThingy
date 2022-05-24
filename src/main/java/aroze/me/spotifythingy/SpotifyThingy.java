f77eaedfea128921839399e436dcffd98de263ba
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-24 20:40:39 +0100

    Initial commit

---
+++ src/main/java/aroze/me/spotifythingy/SpotifyThingy.java
@@ -0,0 +1,17 @@
+package aroze.me.spotifythingy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpotifyThingy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
