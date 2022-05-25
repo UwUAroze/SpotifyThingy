54cffa82fb6a8140e0ed21449d8b137ecfad14aa
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-25 18:43:29 +0100

    Created /pause

---
+++ src/main/java/aroze/me/spotifythingy/commands/PauseCommand.java
@@ -0,0 +1,39 @@
+package aroze.me.spotifythingy.commands;
+
+import aroze.me.spotifythingy.util.ChatUtils;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class PauseCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        Player player = (Player) sender;
+
+        HttpResponse<JsonNode> response = Unirest.get("https://api.spotify.com/v1/me/player/currently-playing")
+                .header("Authorization", "Bearer " + TestAuth.spotifyAuth.get(player.getUniqueId()))
+                .asJson();
+
+        boolean isPlaying = response.getBody().getArray().getJSONObject(0).getBoolean("is_playing");
+
+        if (isPlaying) {
+            HttpResponse<JsonNode> pauseResponse = Unirest.put("https://api.spotify.com/v1/me/player/pause")
+                    .header("Authorization", "Bearer " + TestAuth.spotifyAuth.get(player.getUniqueId()))
+                    .asJson();
+            sender.sendMessage(ChatUtils.color("&7&oPaused Spotify playback"));
+            return true;
+        }
+
+        HttpResponse<JsonNode> playResponse = Unirest.put("https://api.spotify.com/v1/me/player/play")
+                .header("Authorization", "Bearer " + TestAuth.spotifyAuth.get(player.getUniqueId()))
+                .asJson();
+        sender.sendMessage(ChatUtils.color("&7&oAlready paused so resumed Spotify playback"));
+
+        return true;
+    }
+}
