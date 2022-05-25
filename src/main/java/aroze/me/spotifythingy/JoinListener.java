1011a2b9ae2c33bf27d1631a344f6be295b295b7
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-25 16:43:59 +0100

    Fixed Spotify bossbar

--- src/main/java/aroze/me/spotifythingy/JoinListener.java
+++ src/main/java/aroze/me/spotifythingy/JoinListener.java
@@ -5,19 +5,20 @@ import org.bukkit.Bukkit;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
 
-    public boolean onJoin(PlayerJoinEvent e) {
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
         BossBar spotifyPlayer = Bukkit.createBossBar(
                 ChatUtils.color("&#51e285Spotify: &#1fb177Not Connected"),
                 BarColor.GREEN,
                 BarStyle.SOLID
         );
         spotifyPlayer.addPlayer(e.getPlayer());
-        return true;
     }
 
 }
